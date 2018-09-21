d5dab2ba85169949ed6b0245c63c49df01739bca
MirandaTilton <tiltonm@bigfoot.csafe.iastate.edu>
2018-09-21 14:54:12 -0500

    Completed automation script

--- vgg16_augment_onehot_automation.R
+++ vgg16_augment_onehot_automation.R
@@ -1,36 +1,40 @@
 library(magrittr)
+library(lubridate)
+library(stringr)
+library(jpeg)
 library(keras)
+
 use_backend("tensorflow")
 #install_keras()
 
 classes <- c("bowtie", "chevron", "circle", "hexagon", "line",
              "pentagon", "quad", "star", "text", "triangle")
 
+work_dir <- "/models/shoe_nn/TrainedModels"
 start_date <- Sys.Date()
+model_dir <- file.path(work_dir, start_date)
+dir.create(model_dir)
 
-name.file <- function(date, ext) {
-  work_dir <- #this will be the folder to save the models after running
-  path <- paste(work_dir, date, sep = "/")
-  
+name_file <- function(date, ext) {
   pretrained_base = "vgg16"
   mod_type = "onehotaug"
   nclass = paste0(length(classes), "class")
   pixel_size = "256"
   
-  filename <- paste(date, pretrained_base, mod_type, nclass, 
+  filename <- paste(pretrained_base, mod_type, nclass, 
                     pixel_size, sep = "_")
   
-  return(paste(path, filename, ext, sep = ""))
+  file.path(model_dir, filename) %>%
+    paste0(., ext)
 }
 
-conv_base <- application_vgg16(
-  weights = "imagenet",
-  include_top = FALSE,
-  input_shape = c(256, 256, 3)
-)
+processed_img_folder <- list.files("/models/shoe_nn/RProcessedImages/") %>% 
+  as_datetime() %>%
+  max(na.rm = T) %>%
+  gsub("[^0-9\\ ]", "", .) %>%
+  gsub(" ", "-", .)
 
-# This needs to be the home of the three image directories
-base_dir <- #file.path("/models/R_proccessed_images", start_date)
+base_dir <- file.path("/models/shoe_nn/RProcessedImages", processed_img_folder)
 train_dir <- file.path(base_dir, "train")
 validation_dir <- file.path(base_dir, "validation")
 test_dir <- file.path(base_dir, "test")
@@ -39,32 +43,28 @@ n_train <- length(list.files(train_dir))
 n_validation <- length(list.files(validation_dir))
 n_test <- length(list.files(test_dir))
 
-library(stringr)
-fnames <- list.files(train_dir)
-fnames.front <- str_remove(fnames, ".jpg")
-img.loc <- file.path(train_dir, fnames)
-
-library(jpeg)
+img_names <- list.files(train_dir) %>% str_remove(., ",jpg")
+img_loc <- list.files(train_dir, full.names = T)
 
 for (i in 1:n_train) {
   
-  img <- readJPEG(img.loc[i])
+  img <- readJPEG(img_loc[i])
   dim(img) <- c(1, dim(img))
   
-  gen_images <- image_data_generator(rotation_range = 40,
-                                     width_shift_range = 0.05,
-                                     height_shift_range = 0.05,
-                                     shear_range = 0.4,
-                                     zoom_range = 0.1,
-                                     horizontal_flip = TRUE)
+  aug_generator <- image_data_generator(rotation_range = 40,
+                                        width_shift_range = 0.05,
+                                        height_shift_range = 0.05,
+                                        shear_range = 0.4,
+                                        zoom_range = 0.1,
+                                        horizontal_flip = TRUE)
   
   images_iter <- flow_images_from_data(
-    x=img, y=NULL,
-    generator=gen_images,
-    batch_size=1,
-    save_to_dir=train_aug_dir,
-    save_prefix=paste("aug", fnames.front[i], sep="_"),
-    save_format="jpeg"
+    x = img, y = NULL,
+    generator = aug_generator,
+    batch_size = 1,
+    save_to_dir = train_aug_dir,
+    save_prefix = paste("aug", img_names[i], sep="_"),
+    save_format = "jpeg"
   )
   
   reticulate::iter_next(images_iter)
@@ -72,9 +72,15 @@ for (i in 1:n_train) {
   reticulate::iter_next(images_iter)
 }
 
+
+conv_base <- application_vgg16(
+  weights = "imagenet",
+  include_top = FALSE,
+  input_shape = c(256, 256, 3)
+)
+
+
 extract_features2 <- function(directory, sample_count) {
-  library(jpeg)
-  
   features <- array(0, dim = c(sample_count, 8, 8, 512))  
   labels <- array(0, dim = c(sample_count, length(classes)))
   
@@ -104,6 +110,7 @@ train <- extract_features2(train_dir, n_train)
 validation <- extract_features2(validation_dir, n_validation)
 test <- extract_features2(test_dir, n_test)
 
+
 reshape_features <- function(features) {
   array_reshape(features, dim = c(nrow(features), 8 * 8 * 512))
 }
@@ -111,6 +118,7 @@ reshape_features <- function(features) {
 train$features <- reshape_features(train$features)
 validation$features <- reshape_features(validation$features)
 test$features <- reshape_features(test$features)
+
 
 model <- keras_model_sequential() %>% 
   layer_dense(units = 256, activation = "relu", 
@@ -132,15 +140,15 @@ history <- model %>% fit(
 )
 
 
-png(name.file(start_date, ".png"))
+png(name_file(start_date, ".png"))
 plot(history)
 dev.off()
 
-save_model_hdf5(model, name.file(start_date, ".h5"))
+save_model_hdf5(model, name_file(start_date, ".h5"))
 
 preds <- model %>% predict(test$features)
 test_labs <- test$labels
 colnames(preds) <- colnames(test_labs) <- classes
 
-save(preds, test_labs, file = name.file(start_date, ".Rdata"))
-# base::save.image(name.file(start_date, ".RData"))
+save(classes, preds, test_labs, file = name_file(start_date, ".rdata"))
+# base::save.image(name_file(start_date, ".rdata"))
